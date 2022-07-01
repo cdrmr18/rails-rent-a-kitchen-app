53f8146cf55c266fbce1ec83e32aeaec9890b38a
Kathy Tavia <kathy.tavia@gmail.com>
2022-07-01 19:55:41 +0200

    completed listings controller & listing policy

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,22 +1,55 @@
 class ListingsController < ApplicationController
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, except: [:new, :edit, :destroy]
+
   def index
+    @listings = policy_scope(Listing)
   end
 
   def show
   end
 
   def new
+    @listing = Listing.new
+    authorize @listing
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    @listing.user = current_user
+    authorize  @listing
+
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @listing.update(listing_params)
+      redirect_to listing_path(@listing)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @listing.destroy
+    redirect_to listings_path
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+    authorize @listing
+  end
+
+  def listing_params
+    params.require(:listing).permit(:name, :price, :city, :description, :address)
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ApplicationRecord
   has_many :bookings
   has_many :reviews
+  belongs_to :user
 
   validates :price, :name, :city, :description, :address, presence: true
 end

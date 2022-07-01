f73e160bd3b0556da7d4e550760af4c6737c0d4b
Kathy Tavia <kathy.tavia@gmail.com>
2022-07-01 13:40:53 +0200

    added reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
   devise_for :users
   root to: 'pages#home'
   # /listing/:id -- listings/new
   resources :listings do
     # POST   /listings/:listing_id/bookings
     resources :bookings, only: [:create]
+     # POST   /listings/:listing_id/review
+    resources :reviews, only: [:create]
   end
 
-  # /bookings
+  # /bookings --  /bookings/:id
   resources :bookings, only: [:index, :show, :destroy]
+  resources :reviews, only: [:destroy]
 end

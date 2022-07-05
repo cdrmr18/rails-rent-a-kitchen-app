d87886041137d8c5e2a42dac02f9821f6c307f65
Kathy Tavia <kathy.tavia@gmail.com>
2022-07-05 10:17:24 +0200

    added search to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,11 @@ class Listing < ApplicationRecord
   has_many_attached :photos
 
   validates :price, :name, :city, :description, :address, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_city,
+    against: [ :city ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end

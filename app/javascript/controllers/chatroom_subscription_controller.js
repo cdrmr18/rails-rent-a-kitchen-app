7ea6b816469fbc248ac8b9d9663ba7f72310a9ed
Kathy Tavia <kathy.tavia@gmail.com>
2022-07-10 19:48:24 +0200

    started action set up

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus";
+import consumer from "../channels/consumer";
+
+export default class extends Controller {
+  static values = { chatroomId: Number };
+  static targets = ["messages"];
+
+  connect() {
+    console.log(
+      `Subscribe to the chatroom with the id ${this.chatroomIdValue}.`
+    );
+  }
+}
